b8233b7ba35fb89f47c54091b750a8b0527cb4da
Evance Odoyo <odoyoevanc@gmail.com>
2024-07-19 01:27:46 +0300

    user crud

---
+++ app/crud/crud_user.py
@@ -0,0 +1,39 @@
+from sqlalchemy.orm import Session
+from app.models import User
+from app.schemas import UserCreate, UserUpdate
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 20):
+    return db.query(User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: UserCreate):
+    db_user = User(**user.model_dump())
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def update_user(db: Session, user: UserUpdate, user_id: int):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    for key, value in user.model_dump().items():
+        setattr(db_user, key, value)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    db.delete(db_user)
+    db.commit()
+    return db_user
