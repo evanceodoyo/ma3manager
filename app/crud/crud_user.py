6b3ef39d6502f0fc4b3157d11c1650aa7f592610
Evance Odoyo <odoyoevanc@gmail.com>
2024-07-19 03:07:58 +0300

    fix: hash passsword before commit

--- app/crud/crud_user.py
+++ app/crud/crud_user.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from app.models import User
 from app.schemas import UserCreate, UserUpdate
+from app.core.security import get_password_hash
 
 
 def get_user(db: Session, user_id: int):
@@ -16,7 +17,17 @@ def get_users(db: Session, skip: int = 0, limit: int = 20):
 
 
 def create_user(db: Session, user: UserCreate):
-    db_user = User(**user.model_dump())
+    db_user = User(
+        name=user.name,
+        email=user.email,
+        role=user.role,
+        id_number=user.id_number,
+        dob=user.dob,
+        phone_number=user.phone_number,
+        driving_license_number=user.driving_license_number,
+        location_id=user.location_id,
+        password=get_password_hash(user.password)
+    )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
